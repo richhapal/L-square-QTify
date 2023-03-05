0da5b497bd09da9b77f3ec4699ce0c061d7d6771
RICHHAPAL CHOUDHARY <richhapal.16jccs098@jietjodhpur.ac.in>
2023-03-05 15:58:14 +0530

    C3:SearchBar

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Logo } from "../Logo/Logo";
+import { Searchbar } from "../Searchbar/Searchbar";
 import style from "./Navbar.module.css";
 export const Navbar = () => {
      return (
           <div className={style.navbar}>
                <Logo />
+               <Searchbar />
           </div>
      );
 };
--- qtify/src/components/Searchbar/Searchbar.jsx
+++ qtify/src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-
+import style from "./Searchbar.module.css";
 export const Searchbar = () => {
-     return <input type="text" name="search" id="search" />;
+     return (
+          <div className={style.search}>
+               <input className={style.input} type="text" name="search" id="search" placeholder="Search a song of your choice" />
+               <div className={style.searchbutton}>
+                    <img src={require("../../assests/SearchIcon.png")} alt="searchIcons" width={23} height={23} />
+               </div>
+          </div>
+     );
 };
