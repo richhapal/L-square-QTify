b705bfc153fc342c9c92ef3f6efa1ff16fbf233d
RICHHAPAL CHOUDHARY <richhapal.16jccs098@jietjodhpur.ac.in>
2023-03-22 13:48:56 +0530

    hero-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Navbar } from "./components/Navbar/Navbar";
+import HeroSections from "./components/HeroSection/HeroSections";
 
 function App() {
      return (
           <>
                <Navbar />
+               <HeroSections />
           </>
      );
 }
---
+++ qtify/src/components/HeroSection/HeroSections.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import style from "./Hero.module.css";
+const HeroSections = () => {
+     return (
+          <section className={style.heroSection}>
+               <div className={style.heroContent}>
+                    <div className={style.heroText}>
+                         <h1>100 Thousand Songs, ad-free</h1>
+                         <h1>Over thousands podcast episodes</h1>
+                    </div>
+                    <div className={style.heroImage}>
+                         <img src={require("../../assests/vibrating-headphone.png")} alt="heroImage" srcset="" height="212px" width="212px" />
+                    </div>
+               </div>
+          </section>
+     );
+};
+export default HeroSections;
